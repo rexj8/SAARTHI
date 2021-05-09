a41bddb2dd6b91ff269b47208b75cf8326b6c94d
rexj8 <rachit.gla_cs18@gla.ac.in>
2021-05-09 09:03:56 +0530

    DB updated

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var Message = mongoose.model('Message',{
     message : String
 })
 
-var dbUrl = 'mongodb://localhost:27017/SAARTHI'
+var dbUrl = 'mongodb+srv://Rexj8:D8g5eGZwLaiTdTKO@nodejs-login.pv5db.mongodb.net/SAARTHI?retryWrites=true&w=majority'
 
 app.get('/world/messages', (req, res) => {
     Message.find({},(err, messages)=> {
